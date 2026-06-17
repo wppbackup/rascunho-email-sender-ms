e999655bc55615dd92b9c3dbff16937e38518ed4
wppbackup <wppbackup.8162.8797@gmail.com>
2026-06-17 09:39:46 -0300

    fix queue not showing up on LavinMQ manager

--- user-mail-sender-ms/email/src/main/java/dev/java10x/email/config/RabbitMq.java
+++ user-mail-sender-ms/email/src/main/java/dev/java10x/email/config/RabbitMq.java
@@ -1,13 +1,23 @@
 package dev.java10x.email.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMq {
     private final String queueName = "email-queue";
 
+    @Bean
     public Queue queue() {
         return new Queue(queueName, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 }
